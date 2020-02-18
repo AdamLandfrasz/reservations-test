91bbd6a3baa393a378c6e330f7af07a2b35b65a5
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-02-18 11:14:14 +0100

    login page class added

---
+++ src/main/java/pages/ReservationsLogin.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ReservationsLogin extends BasePage {
+    @FindBy(xpath = "//input[@id='username']")
+    WebElement usernameField;
+    @FindBy(xpath = "//input[@id='password']")
+    WebElement passwordField;
+    @FindBy(xpath = "//button[text()='Submit']")
+    WebElement submitButton;
+
+    public ReservationsLogin(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(this.driver, this);
+    }
+}
