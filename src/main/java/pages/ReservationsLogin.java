336d3ab22e34e1823ae2b97d3f258e4199d5b26e
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-02-20 12:23:32 +0100

    new base test structure

--- src/main/java/pages/ReservationsLogin.java
+++ src/main/java/pages/ReservationsLogin.java
@@ -1,16 +1,11 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ReservationsLogin extends BasePage {
-    @FindBy(xpath = "//input[@id='username']//ancestor-or-self::form")
-    WebElement loginForm;
     @FindBy(xpath = "//input[@id='username']")
     WebElement usernameField;
     @FindBy(xpath = "//input[@id='password']")
@@ -37,7 +32,7 @@ public class ReservationsLogin extends BasePage {
     }
 
     public void logout() {
-        By loginPageForm = By.xpath("//input[@id='username']//ancestor-or-self::form");
+        By loginPageForm = By.xpath("//form[//button[text()='Submit']]");
 
         wait.until(ExpectedConditions.elementToBeClickable(logoutButton)).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(loginPageForm));
@@ -46,7 +41,7 @@ public class ReservationsLogin extends BasePage {
     public boolean isLogoutButtonPresent() {
         try {
             return wait.until(ExpectedConditions.elementToBeClickable(logoutButton)).isDisplayed();
-        } catch (NoSuchElementException e) {
+        } catch (NoSuchElementException | TimeoutException e) {
             return false;
         }
     }
@@ -56,7 +51,17 @@ public class ReservationsLogin extends BasePage {
 
         try {
             return wait.until(ExpectedConditions.presenceOfElementLocated(loginPageForm)).isDisplayed();
-        } catch (NoSuchElementException e) {
+        } catch (NoSuchElementException | TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean isAlertPresent() {
+        By loginPageForm = By.xpath("//div[@id='alert']");
+
+        try {
+            return wait.until(ExpectedConditions.presenceOfElementLocated(loginPageForm)).isDisplayed();
+        } catch (NoSuchElementException | TimeoutException e) {
             return false;
         }
     }
